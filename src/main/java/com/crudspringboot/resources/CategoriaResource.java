102cceb35b62ccded231571d0a8cba42e4f7d03e
Otávio Cavalcanti <otaviooasc@gmail.com>
2020-11-09 21:10:50 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/crudspringboot/resources/CategoriaResource.java
+++ src/main/java/com/crudspringboot/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.crudspringboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.crudspringboot.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "FUNFOU";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> list = new ArrayList();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 }

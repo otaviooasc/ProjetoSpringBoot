2c17bb1d42eb19abda27c816e654e054ce7607ea
Otávio Cavalcanti <otaviooasc@gmail.com>
2020-11-24 20:41:38 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/crudspringboot/CrudspringbootApplication.java
+++ src/main/java/com/crudspringboot/CrudspringbootApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.crudspringboot.domain.Categoria;
+import com.crudspringboot.domain.Produto;
 import com.crudspringboot.repositories.CategoriaRepository;
+import com.crudspringboot.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CrudspringbootApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudspringbootApplication.class, args);
@@ -24,6 +29,18 @@ public class CrudspringbootApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "pendrive_1T", 200.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
